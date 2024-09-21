26a1c6687cd4b6e2058383a9a9aa4f6bb1a5c1eb
rgarlando23 <ralphgarlando.rg@gmail.com>
2024-09-21 15:21:53 +0800

    Update server.js

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 
 // Serve the main page with the color passed as a query parameter
 app.get('/', (req, res) => {
-    const color = req.query.color || '#FFFFFF';  // Default to white
+    const color = req.query.color || '#FF12312312321FFFF';  // Default to white
     res.render('index', { color });
 });
 
